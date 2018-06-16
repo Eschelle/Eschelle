0a653da38d1ac3c8fb3b2e306c1a718d778479c0
s0cks <tasman.vose@gmail.com>
2018-06-16 06:50:21 -0600

    Ignore bin

--- bin/src/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/tasmanvose/Desktop/Development Projects/Eschelle/src
-# Build directory: /Users/tasmanvose/Desktop/Development Projects/Eschelle/bin/src
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
